8ed14fc0bffff82760e15d5361fae1af345add14
Andy Baker <andy@andybak.net>
2024-01-23 19:18:56 +0000

    Extract metadata from imported file

---
+++ Packages/open-brush-unity-tools/Runtime/Scripts/BatchMetadata.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine.Serialization;
+
+public class BatchMetadata : UnityEngine.MonoBehaviour
+{
+    [Serializable]
+    public class SubsetMetadata
+    {
+        public uint m_HeadTimestampMs;
+        public uint m_TailTimestampMs;
+        public uint m_StartVertIndex;
+        public uint m_VertLength;
+        public uint m_Group;
+    }
+
+    public List<SubsetMetadata> m_Subsets;
+}
--- Packages/open-brush-unity-tools/Runtime/Scripts/ObImportPlugin.cs
+++ Packages/open-brush-unity-tools/Runtime/Scripts/ObImportPlugin.cs
@@ -1,5 +1,9 @@
-﻿using UnityGLTF.Plugins;
+﻿using System;
+using System.Collections.Generic;
+using GLTF.Extensions;
+using UnityGLTF.Plugins;
 using GLTF.Schema;
+using Newtonsoft.Json.Linq;
 using UnityEngine;
 
 namespace OpenBrushUnityTools
@@ -17,14 +21,51 @@ namespace OpenBrushUnityTools
 
         public class ObImportExtensionConfig : GLTFImportPluginContext
         {
+
+            private MaterialRemapping m_MaterialDictionary;
+
+            public override void OnBeforeImport()
+            {
+                m_MaterialDictionary = Resources.Load<MaterialRemapping>("MaterialRemapping");
+            }
+
             public override void OnAfterImportNode(Node node, int nodeIndex, GameObject nodeObject)
             {
-                var materialDictionary = Resources.Load<MaterialRemapping>("MaterialRemapping");
+                var strokeJson = node?.Mesh?.Value?.Extras?["ICOSA_strokeInfo"];
+                if (strokeJson != null)
+                {
+                    var reader = strokeJson.CreateReader();
+                    var strokeInfo = reader.ReadAsDictionary(() => reader.ReadAsString());
+                    var metadata = nodeObject.AddComponent<StrokeMetadata>();
+                    UInt32.TryParse(strokeInfo["HeadTimestampMs"], out metadata.m_HeadTimestampMs);
+                    UInt32.TryParse(strokeInfo["TailTimestampMs"], out metadata.m_TailTimestampMs);
+                }
+                else
+                {
+                    // Is there ever more than one primitive that could contain metadata?
+                    var batchJson = node?.Mesh?.Value?.Primitives[0].Extras?["ICOSA_batchInfo"];
+                    if (batchJson != null)
+                    {
+                        var metadata = nodeObject.AddComponent<BatchMetadata>();
+                        metadata.m_Subsets = new List<BatchMetadata.SubsetMetadata>();
+                        foreach (var metadataItem in batchJson)
+                        {
+                            var subsetMetadata = new BatchMetadata.SubsetMetadata();
+                            UInt32.TryParse(metadataItem["HeadTimestampMs"].Value<string>(), out subsetMetadata.m_HeadTimestampMs);
+                            UInt32.TryParse(metadataItem["TailTimestampMs"].Value<string>(), out subsetMetadata.m_TailTimestampMs);
+                            UInt32.TryParse(metadataItem["StartVertIndex"].Value<string>(), out subsetMetadata.m_StartVertIndex);
+                            UInt32.TryParse(metadataItem["VertLength"].Value<string>(), out subsetMetadata.m_VertLength);
+                            UInt32.TryParse(metadataItem["Group"].Value<string>(), out subsetMetadata.m_Group);
+                            metadata.m_Subsets.Add(subsetMetadata);
+                        }
+                    }
+                }
+
                 var mr = nodeObject.GetComponent<MeshRenderer>();
                 if (mr != null)
                 {
                     var existingMaterialName = mr.sharedMaterial.name.Replace("(Instance)", "").Trim();
-                    var mat = materialDictionary.GetMaterial(existingMaterialName);
+                    var mat = m_MaterialDictionary.GetMaterial(existingMaterialName);
                     if (mat != null)
                     {
                         mr.sharedMaterial = mat;
