3956a013cb5046f8909e3feb495555ee56e02191
Andy Baker <andy@andybak.net>
2024-01-23 19:18:56 +0000

    Extract metadata from imported file

---
+++ Runtime/Scripts/StrokeMetadata.cs
@@ -0,0 +1,8 @@
+﻿using System.Globalization;
+using UnityEngine.Serialization;
+
+public class StrokeMetadata : UnityEngine.MonoBehaviour
+{
+    public uint m_HeadTimestampMs;
+    public uint m_TailTimestampMs;
+}
