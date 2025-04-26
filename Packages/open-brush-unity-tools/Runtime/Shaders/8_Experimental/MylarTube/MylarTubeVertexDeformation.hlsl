1426ad4ff3a46a19e94dd0f83cd873f42f46ee50
Andy Baker <andy@andybak.net>
2025-04-26 11:59:37 +0100

    Better mylar

---
+++ Packages/open-brush-unity-tools/Runtime/Shaders/8_Experimental/MylarTube/MylarTubeVertexDeformation.hlsl
@@ -0,0 +1,19 @@
+﻿#ifndef MYLARTUBE_VERTEX_DEFORMATION
+#define MYLARTUBE_VERTEX_DEFORMATION
+
+void mylarTubeVertex_float(
+    float3 position,
+    float3 normal,
+    float radius,
+    float u,
+    float squeeze_amount,
+    out float3 modified_position,
+    out float3 modified_normal)
+{
+    float squeeze = sin(u * 3.14159);
+    float3 squeeze_displacement = radius * normal * squeeze;
+    modified_position = position - (squeeze_displacement * squeeze_amount);
+    modified_normal = normalize(normal + squeeze_displacement * 2.5);
+}
+
+#endif
