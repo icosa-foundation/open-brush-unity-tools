05c8d29af91256df086937d8213da1af30b661ab
Andy Baker <andy@andybak.net>
2026-05-20 16:06:50 +0100

    fix light wire when not baked

---
+++ Packages/open-brush-unity-tools/Runtime/Shaders/6_StandardSpecials/LightWire/LightWireVertexDeformation.hlsl
@@ -0,0 +1,19 @@
+﻿#ifndef MYLARTUBE_VERTEX_DEFORMATION
+#define MYLARTUBE_VERTEX_DEFORMATION
+
+void lightWireVertex_float(
+    float3 position,
+    float3 normal,
+    float4 uv,
+    out float3 modified_position,
+    out float3 modified_normal)
+{
+    float radius = uv.z;
+    float envelope = sin(fmod( uv.x * 2, 1.0f) * 3.14159);
+    float lights = envelope < .15 ? 1 : 0;
+    radius *= 0.9;
+    modified_position = position += normal * lights * radius;
+    modified_normal = normal;
+}
+
+#endif
